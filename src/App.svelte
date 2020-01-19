e38fdccf0be799374a6c1412ed08bcfb0f58b830
sidv <sidharthv96@gmail.com>
2020-01-19 23:07:46 +0530

    Update todos

--- src/App.svelte
+++ src/App.svelte
@@ -43,11 +43,12 @@
     uid = 1;
     todos = [
       { id: uid++, done: false, description: "write some docs" },
-      { id: uid++, done: false, description: "start writing blog post" },
+      { id: uid++, done: true, description: "[Carrom] Ruby backend" },
+      { id: uid++, done: true, description: "[Carrom] Svelte frontend" },
       { id: uid++, done: true, description: "buy some milk" },
       { id: uid++, done: false, description: "mow the lawn" },
       { id: uid++, done: false, description: "feed the turtle" },
-      { id: uid++, done: true, description: "fix some bugs" }
+      { id: uid++, done: false, description: "fix some bugs" }
     ];
     updateStorage();
   }
